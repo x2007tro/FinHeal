9abd34c88e057b601cda839a4d061bcb30a2bc30
x2007tro <kemin.business@gmail.com>
2022-03-06 21:44:28 -0400

    remove empty div

--- iUI/pf_ipt_ppm.R
+++ iUI/pf_ipt_ppm.R
@@ -6,18 +6,15 @@ pf_ipt_ppm_tp <- tabPanel(
   fluidRow(
     column(
       12,
-      
-      tags$div(
-        do.call(tabsetPanel, c(id = "pf_ipt_ppm_tab", lapply(1:nrow(property_shownact), function(i){
-          
-          tabPanel(
-            paste(property_shownact$name[i], "(",property_shownact$operation_type[i],")"),
-            uiOutput(paste0("pf_ipt_ppm_", property_shownact$id[i]))
-          )
-          
-        })))
-      )
-      
+
+      do.call(tabsetPanel, c(id = "pf_ipt_ppm_tab", lapply(1:nrow(property_shownact), function(i){
+        
+        tabPanel(
+          paste(property_shownact$name[i], "(",property_shownact$operation_type[i],")"),
+          uiOutput(paste0("pf_ipt_ppm_", property_shownact$id[i]))
+        )
+        
+      })))
     )
   )
 )
