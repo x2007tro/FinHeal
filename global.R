05321d4f77eabb253a29a0dd33db896badcebb4f
x2007tro <kemin.business@gmail.com>
2022-03-07 22:11:12 -0400

    adjust color pallete

--- global.R
+++ global.R
@@ -10,7 +10,7 @@ entry_wid_m <- "180px"
 entry_wid_l <- "275px"
 entry_wid_xl <- "500px"
 plot_font_color <- "#808080"
-brewed_colors <- rep(RColorBrewer::brewer.pal(n = 8, name = "Pastel1"), 100)
+brewed_colors <- rep(RColorBrewer::brewer.pal(n = 9, name = "Set3"), 100)
 get_palette <- colorRampPalette(RColorBrewer::brewer.pal(8, name = 'Pastel1'))
 
 #
